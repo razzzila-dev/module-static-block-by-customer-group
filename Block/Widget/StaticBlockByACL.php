a7ce1690c7efc59d5c905ad36f4eaf8ca2aa034d
dev <mg3Ktcy53K2Nh7X9T2Vc>
2020-02-03 23:40:33 +0300

    + module registration + static block by acl widget

---
+++ Block/Widget/StaticBlockByACL.php
@@ -0,0 +1,105 @@
+<?php
+namespace Razzzila\StaticBlockACL\Block\Widget;
+
+use Magento\Framework\{
+    App\Http\Context as HttpContext,
+    View\Element\Template\Context as TemplateContext
+};
+use Magento\Cms\{
+    Block\Widget\Block,
+    Model\Template\FilterProvider,
+    Model\BlockFactory
+};
+use Magento\Customer\Model\Context as CustomerContext;
+
+/**
+ * Class StaticBlockByACL
+ *
+ * @package Razzzila\StaticBlockACL\Block\Widget
+ */
+class StaticBlockByACL extends Block
+{
+    /** @var HttpContext */
+    protected $httpContext;
+    
+    /**
+     * DynamicGroupData constructor.
+     *
+     * @param  TemplateContext  $context
+     * @param  FilterProvider  $filterProvider
+     * @param  BlockFactory  $blockFactory
+     * @param  HttpContext  $httpContext
+     * @param  array  $data
+     */
+    public function __construct(
+        TemplateContext $context,
+        FilterProvider $filterProvider,
+        BlockFactory $blockFactory,
+        HttpContext $httpContext,
+        array $data = []
+    ) {
+        $this->httpContext = $httpContext;
+        
+        parent::__construct(
+            $context,
+            $filterProvider,
+            $blockFactory,
+            $data
+        );
+    }
+
+    /**
+     * @return string
+     */
+    protected function _toHtml() {
+        return $this->isRenderingAllowed() ? parent::_toHtml() : '';
+    }
+
+    /**
+     * @return bool
+     */
+    protected function isRenderingAllowed() {
+        return $this->isAllowedForAll()
+            || array_search(
+                $this->getCurrentCustomerGroup(),
+                $this->getAllowedCustomerGroups()
+            ) !==  false;
+    }
+
+    /**
+     * @return bool
+     */
+    protected function isAllowedForAll() {
+        return $this->getData('customer_groups_ids') === '';
+    }
+
+    /**
+     * @return mixed|null
+     */
+    protected function getCurrentCustomerGroup() {
+        return $this->httpContext->getValue(
+            CustomerContext::CONTEXT_GROUP
+        );
+    }
+
+    /**
+     * @return array
+     */
+    protected function getAllowedCustomerGroups() {
+        return explode(
+            ',',
+            $this->getData('customer_groups_ids')
+        );
+    }
+
+    /**
+     * @return array
+     */
+    public function getIdentities() {
+        $customerGroup = $this->getCurrentCustomerGroup();
+        $identities = parent::getIdentities();
+        $identities[] = 'customer_group_' . $customerGroup;
+        
+        return $identities;
+    }
+}
---
+++ registration.php
@@ -0,0 +1,8 @@
+<?php
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Razzzila_StaticBlockACL',
+    __DIR__
+);
